b14cbb76a45440aaaa06f46991591475113951ca
Corrado Raffaelli <raffaelli.corrado@gmail.com>
2017-01-05 10:10:03 +0100

    Project

---
+++ Assets/Scripts/ButtonChange.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using Vuforia;
+
+public class ButtonChange : MonoBehaviour, IVirtualButtonEventHandler {
+    
+    public bool enabled = true;
+
+    public GameObject ulObj;
+    public GameObject urOgj;
+
+    public VirtualButtonBehaviour[] buttons;
+
+	void Start () {
+	    if (buttons != null && buttons.Length != 0)
+        {
+            for (int i = 0; i < buttons.Length; i++)
+            {
+                if (buttons[i] != null)
+                    buttons[i].RegisterEventHandler(this);
+            }
+        }
+	}
+	
+    public void OnButtonPressed(VirtualButtonAbstractBehaviour vb)
+    {
+        if (vb.gameObject.name == "VirtualButtonUL")
+        {
+            ulObj.SetActive(!ulObj.activeSelf);
+        }
+
+        if (vb.gameObject.name == "VirtualButtonUR")
+        {
+            urOgj.SetActive(!urOgj.activeSelf);
+        }
+    }
+
+    public void OnButtonReleased(VirtualButtonAbstractBehaviour vb)
+    {
+
+    }
+}
